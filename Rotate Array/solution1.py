59afe76507e46c5d830ccaa2153aa100e4b243b7
lsmail Qayyum <152914864+IsmailQayyum@users.noreply.github.com>
2024-06-09 14:31:08 -0700

    Delete Rotate Array/solution1.py

--- Rotate Array/solution1.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        for i in range(k):
-            last_element = nums.pop(len(nums)-1)
-            nums.insert(0,last_element)
